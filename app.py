cbff792007fd093e55e811174cf44cdc3b2e72da
Joaquim Perez <jivkim@gmail.com>
2024-01-27 13:42:06 +0100

    add anotation text, x must be a timestamp

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-from dash import Dash, html, dcc, callback, Output, Input
+from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
+from datetime import datetime
 
 df = pd.read_csv('data.csv')
+
+df['time'] = pd.to_datetime(df['time'])
 
 app = Dash(__name__)
 
@@ -15,7 +18,12 @@ fig = px.line(
     markers=True
 )
 
-fig.add_vline(x='2024-01-26 14:00:00')
+fig.add_vline(
+    x=datetime.timestamp(pd.to_datetime('2024-01-26 14:00:00')) * 1000,
+    # x=1706274000000,
+    annotation_text='Action',
+    annotation_position='top right'
+)
 
 app.layout = html.Div([
     dcc.Graph(figure=fig)
